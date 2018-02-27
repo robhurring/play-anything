c5084c67aa6cc923c9ca0b6b6dbdf33f18a30c89
robhurring <robhurring@gmail.com>
2018-02-26 22:53:55 -0500

    Add basic perms

--- controllers/player.js
+++ controllers/player.js
@@ -1,3 +1,56 @@
+const SpotifyWebApi = require("spotify-web-api-node");
+
+exports.spotifyMiddleware = function(req, res, next) {
+  const app = req.app;
+  const credentials = app.get("spotify.credentials");
+  const api = (req.spotify = new SpotifyWebApi(credentials));
+
+  api.refreshAccessToken().then(
+    function(data) {
+      const token = data.body["access_token"];
+      console.log("refreshed token");
+      app.set(
+        "spotify.credentials",
+        Object.assign({}, credentials, { accessToken: token })
+      );
+      api.setAccessToken(token);
+      next();
+    },
+    function(err) {
+      console.log("token refresh failed", err);
+      res.status(500).json({
+        error: "could not refresh token"
+      });
+    }
+  );
+};
+
+exports.controlMiddleware = function(req, res, next) {
+  const perms = req.app.get("permissions");
+  console.log("control=", perms.control);
+
+  if(!perms.control) {
+    return res.status(400).json({
+      error: "Control disabled"
+    })
+  }
+
+  next();
+}
+
+exports.statusMiddleware = function(req, res, next) {
+  const perms = req.app.get("permissions");
+  console.log("status=", perms.status);
+
+  if(!perms.status) {
+    return res.status(400).json({
+      error: "status disabled"
+    })
+  }
+
+  next();
+}
+
 exports.nextTrack = function(req, res) {
   req.spotify.skipToNext().then(
     function(data) {
@@ -7,7 +60,7 @@ exports.nextTrack = function(req, res) {
       console.error(err);
       res.status(500).json({
         error: err
-      })
+      });
     }
   );
 };
@@ -21,15 +74,15 @@ exports.previousTrack = function(req, res) {
       console.error(error);
       res.status(500).json({
         error
-      })
+      });
     }
   );
 };
 
-exports.state = function(req, res) {
+exports.status = function(req, res) {
   req.spotify.getMyCurrentPlaybackState().then(
     function(data) {
-      const track = data.body['item'];
+      const track = data.body["item"];
 
       res.status(200).json({
         progress_ms: data.body.progress_ms,
@@ -41,7 +94,7 @@ exports.state = function(req, res) {
       console.error(err);
       res.status(500).json({
         error: err
-      })
+      });
     }
   );
 };
@@ -68,7 +121,7 @@ exports.play = function(req, res) {
       console.error(err);
       res.status(500).json({
         error: err
-      })
+      });
     }
   );
 };
--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-var express = require("express"),
+const express = require("express"),
   http = require("http"),
   errorhandler = require("errorhandler"),
   static = require("serve-static"),
   morgan = require("morgan"),
   bodyParser = require("body-parser"),
-  SpotifyWebApi = require("spotify-web-api-node"),
   player = require("./controllers/player");
 
-var app = express();
+const app = express();
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
@@ -19,6 +18,11 @@ if (process.env.NODE_ENV !== "production") {
 
 app.set("port", process.env.PORT || 3000);
 
+app.set("permissions", {
+  control: process.env.ENABLE_CONTROL==="true",
+  status: process.env.ENABLE_STATUS==="true"
+});
+
 app.set("spotify.credentials", {
   clientId: process.env.SPOTIFY_CLIENT_ID,
   clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
@@ -29,34 +33,12 @@ app.set("spotify.credentials", {
 app.use(bodyParser.json());
 app.use(static("public/", {}));
 
-app.use("/player/*", function(req, res, next) {
-  const credentials = app.get("spotify.credentials");
-  const api = req.spotify = new SpotifyWebApi(credentials);
+app.use("/player/*", player.spotifyMiddleware);
 
-  api.refreshAccessToken().then(
-    function(data) {
-      const token = data.body["access_token"];
-      console.log("refreshed token");
-      app.set(
-        "spotify.credentials",
-        Object.assign({}, credentials, { accessToken: token })
-      );
-      api.setAccessToken(token);
-      next();
-    },
-    function(err) {
-      console.log("token refresh failed", err);
-      res.status(500).json({
-        error: "could not refresh token"
-      });
-    }
-  );
-});
-
-app.get("/player/next", player.nextTrack);
-app.get("/player/prev", player.previousTrack);
-app.get("/player/state", player.state);
-app.put("/player/play", player.play);
+app.get("/player/next", [player.controlMiddleware], player.nextTrack);
+app.get("/player/prev", [player.controlMiddleware],player.previousTrack);
+app.get("/player/status", [player.statusMiddleware], player.status);
+app.put("/player/play", [player.controlMiddleware],player.play);
 
 http.createServer(app).listen(app.get("port"), function() {
   console.log("Express server listening on port " + app.get("port"));
